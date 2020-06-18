ebb1ec368ec0228e0be866bb8210ed8706270a3e
448335044 <448335044@qq.com>
2020-06-18 23:45:08 +0800

    header

---
+++ my-blog/src/home.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="page">
+    <div class='header'>
+        <div>
+            <img src="" alt="">
+            <span>博客王</span>
+        </div>
+        <div class="nav">
+            <div>首页</div>
+            <div>技术</div>
+            <div>关于我</div>
+        </div>
+        <div class="login">
+            <div>登录</div>
+            <div>注册</div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.page{
+    font-size: 20px;
+    color:#fff;
+}
+.header{
+    width:100%;
+    height:80px;
+    background-color:aqua;
+    display: flex;
+    align-items: center;
+}
+.header>div{
+    margin: 0 20px;
+}
+.nav{
+    display: flex;
+    flex: 1;
+}
+.login{
+    display: flex;
+}
+</style>
